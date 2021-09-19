e3cdd786818e36fd2e2e693b514b5248ca89a022
Nhi Nguyen <nhinguyen@Nhis-MacBook-Pro.local>
2021-09-19 17:27:03 -0400

    typo fixed

--- test_authorization.py
+++ test_authorization.py
@@ -7,14 +7,14 @@ from authorization import isAuthorized
 
 def test_isAuthorized():
     #return 0 if account doesnt exist
-    assert isAuthorized('nhi34', 'nhiguyen') == 0
-    assert isAuthorized('', 'nhiguyen') == 0
-    assert isAuthorized('nhi34', 'nhiguyen') == 0
-    assert isAuthorized('nhi34', 'nhiguyen') == 0
+    assert isAuthorized('anessa23', 'An23@abc1') == 0
+    assert isAuthorized('jim2301', 'Jim@2a53f') == 0
+    assert isAuthorized('panther902', 'P4na3.def') == 0
+    assert isAuthorized('kelly324', 'kElly34.xyz') == 0
 
     #return 1 if account already exists
-    assert isAuthorized('kevin23', 'Keva.421') == 1 #Change these values into any account you have in your database
-    assert isAuthorized('nhi34', 'nhiguyen') == 1 #Or sign up with these accounts before testing
+    assert isAuthorized('kevin23', 'Keva.2421') == 1 #Change these values into any account you have in your database
+    assert isAuthorized('nhi34', 'nhi@nguyen35') == 1 #Or sign up with these accounts before testing
 
 
 

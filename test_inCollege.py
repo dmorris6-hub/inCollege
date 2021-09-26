28ce25e6cb3bee6aa628fecce3d6254fadb93ab1
Brian Naklycky <69728590+brianakl@users.noreply.github.com>
2021-09-26 14:11:16 -0400

    pytest

--- test_inCollege.py
+++ test_inCollege.py
@@ -1,5 +1,29 @@
 import pytest
+from authorization import isAuthorized
 from add_user import validatePassword, canAdd
+
+
+#check if isAuthorize correctly detects account already exists in the database
+#for accounts that return 1, change the username and password into what you have in your database
+@pytest.mark.parametrize('username, password, result',
+    #return 0 if account doesnt exist
+    [
+                            ('anessa23', 'An23@abc1', 0),
+                            ('jim2301', 'Jim@2a53f', 0),
+                            ('panther902', 'P4na3.def', 0),
+                            ('kelly324', 'kElly34.xyz',0),
+    #return 1 if account already exists
+    #Change these values into any account you have in your database
+    #Or sign up with these accounts before testing
+                            ('kevin23', 'Keva.2421',1),
+                            ('nhi34', 'Nhi@nguyen35',1)
+    ]
+
+)
+def test_isAuthorized(username, password, result):
+    assert isAuthorized(username, password) == result
+
+
 
 #this function checks if the password is valid
 @pytest.mark.parametrize('password, result',
@@ -10,8 +34,7 @@ from add_user import validatePassword, canAdd
                             ('abcdefg8!', 0),
                             ('TooLong_Pa55word',0),
                             ('missingcap1!',0),
-                            ('No-nonAlph5',0),
-                            ('',0),                            
+                            ('NononAlph5',0),                          
                             #valid passwords
                             ('Mark@1234',1),
                             ('aneSsa.9',1),
@@ -36,3 +59,5 @@ def test_canAdd():
     #if the number of accounts is more than 5, return false
     assert(canAdd([(5, )]) == 0)
     assert(canAdd([(6, )]) == 0)
+
+
